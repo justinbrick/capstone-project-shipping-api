06d18a03a27feed458895bda33f70c2da3df6123
Justin <justinbrick1@gmail.com>
2024-02-29 18:29:25 -0500

    check same

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -2,4 +2,4 @@ from os import environ
 import sqlite3
 
 DATABASE_URL = environ.get("DATABASE_URL", "sqlite:///./shipping.db")
-connection: sqlite3.Connection = sqlite3.connect(check_same_thread=False)
+connection: sqlite3.Connection = sqlite3.connect(DATABASE_URL, check_same_thread=False)
