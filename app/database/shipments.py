e3515ba23c6844c2796c2a291cd8ef298d22b26e
Justin <justinbrick1@gmail.com>
2024-02-29 20:11:01 -0500

    get shipment from API

--- app/database/shipments.py
+++ app/database/shipments.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from ..database import connection
 from ..models.shipment import Shipment
 
@@ -19,3 +20,13 @@ def save_shipment(shipment: Shipment):
     Save the shipment to the database.
     """
     connection.execute("INSERT INTO shipments (shipment_id, order_id, shipping_address, provider, provider_shipment_id) VALUES (?, ?, ?, ?, ?)", (str(shipment.shipment_id), str(shipment.order_id), shipment.shipping_address, shipment.provider, shipment.provider_shipment_id))
+
+
+def get_shipment(shipment_id: UUID) -> Shipment:
+    """
+    Get the shipment from the database, using the shipment ID.
+    """
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM shipments WHERE shipment_id=?", (str(shipment_id),))
+    shipment_row = cursor.fetchone()
+    return Shipment(shipment_id=shipment_row[0], order_id=shipment_row[1], shipping_address=shipment_row[2], provider=shipment_row[3], provider_shipment_id=shipment_row[4])
--- app/routers/shipments.py
+++ app/routers/shipments.py
@@ -1,20 +1,23 @@
 from fastapi import APIRouter
 from uuid import UUID
 
+
+
 from ..models.shipment import ShipmentRequest, Shipment, create_shipment
-from ..database.shipments import save_shipment
+from ..database import shipments as shipment_db
 
 router = APIRouter()
 
 @router.post("/")
 async def create_shipment_request(request: ShipmentRequest) -> Shipment:
     shipment = create_shipment(request)
-    save_shipment(shipment)
+    shipment_db.save_shipment(shipment)
     return shipment
 
 @router.get("/{shipment_id}")
-async def get_shipment(shipment_id: UUID):
-    return "Shipment."
+async def get_shipment_request(shipment_id: UUID) -> Shipment:
+    shipment = shipment_db.get_shipment(shipment_id)
+    return shipment
 
 @router.get("/{shipment_id}/status")
 async def get_shipment_status(shipment_id: UUID):
