87958882d0a1015be6f435a0750f0b8ba6d6c20c
Justin Brickley <justin@justin.directory>
2024-03-14 19:26:12 -0400

    raising http exceptions on not found in database, stub changes

--- app/routers/shipments.py
+++ app/routers/shipments.py
@@ -3,6 +3,7 @@ from uuid import UUID
 
 from ..models.shipment import ShipmentRequest, Shipment, create_shipment
 from ..database import shipments as shipment_db
+from ..database import ColumnNotFoundException
 
 router = APIRouter()
 
@@ -14,21 +15,31 @@ async def create_shipment_request(request: ShipmentRequest) -> Shipment:
 
 @router.get("/{shipment_id}")
 async def get_shipment_request(shipment_id: UUID) -> Shipment:
+    """
+    Get the shipment using a specific shipment ID.
+    This is the internal shipment - not the status of the shipment.
+    """
     try:
         shipment = shipment_db.get_shipment(shipment_id)
         return shipment
-    except Exception:
-        raise HTTPException(status_code=404)
+    except ColumnNotFoundException:
+        raise HTTPException(status_code=404, detail=f"Could not find a shipment @ \"{shipment_id}\"")
 
 @router.get("/{shipment_id}/status")
 async def get_shipment_status(shipment_id: UUID):
-    shipment = shipment_db.get_shipment(shipment_id)
+    """
+    Get the shipment status for a specific shipment ID.
+    Due to varying providers, this is a delegate request. As a result, it is volatile depending on the provider.
+    """
+    shipment = await get_shipment_request(shipment_id)
     match shipment.provider:
         case "internal":
             status = shipment_db.get_shipment_status(shipment_id)
         case "ups":
             pass
         case "fedex":
+            pass
+        case "usps":
             pass
         case _:
             pass
