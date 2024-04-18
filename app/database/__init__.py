c9f9a26d61193ce65681ce24ad4b2b1cde8d34fa
Justin <justinbrick1@gmail.com>
2024-04-18 12:06:29 -0400

    fix test logic in code - Fixed the code to not check for pytest, and instead use environment variables.

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -7,17 +7,13 @@ __author__ = "Justin B. (justin@justin.directory)"
 from os import environ
 from sys import modules
 import warnings
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 DATABASE_URL = environ.get(
     "DATABASE_URL", "sqlite:///shipping.db?check_same_thread=False")
 
-# If pytest is running, use an in-memory database
-if "pytest" in modules:
-    print("Using in-memory database for testing.")
-    DATABASE_URL = "sqlite:///:memory:?check_same_thread=False"
 
-engine = create_engine(DATABASE_URL)
+engine: Engine = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(engine)
 session = Session()
