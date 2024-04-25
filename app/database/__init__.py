00067abf1cc15e079fc56173dc3b2ed047033f84
Justin <justinbrick1@gmail.com>
2024-04-25 13:20:08 -0400

    set pool recycle on sql connections

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -14,6 +14,12 @@ DATABASE_URL = environ.get(
     "DATABASE_URL", "sqlite:///shipping.db?check_same_thread=False")
 
 
-engine: Engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True)
+engine: Engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True,
+    pool_recycle=60
+)
+
 Session = sessionmaker(engine)
 session = Session()
